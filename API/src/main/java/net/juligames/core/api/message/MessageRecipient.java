9289573d5767d0a4e057d416948b790327c208d3
TDR <tdr.minecraft@gmail.com>
2022-11-19 13:01:09 +0100

    add CorePostScript

--- API/src/main/java/net/juligames/core/api/message/MessageRecipient.java
+++ API/src/main/java/net/juligames/core/api/message/MessageRecipient.java
@@ -1,8 +1,24 @@
 package net.juligames.core.api.message;
 
+import org.jetbrains.annotations.ApiStatus;
+
 /**
  * @author Ture Bentzin
  * 18.11.2022
  */
 public interface MessageRecipient {
+
+    /**
+     * @return A human-readable name that defines this recipient
+     */
+    String getName();
+    void deliver(Message message);
+
+    @ApiStatus.Internal
+    @Deprecated
+    /**
+     * delivers a miniMessage string to the recipient
+     *
+     */
+    void deliver(String miniMessage);
 }
---
+++ Core/src/main/java/net/juligames/core/jdbi/CoreMessagePostScript.java
@@ -0,0 +1,51 @@
+package net.juligames.core.jdbi;
+
+import net.juligames.core.api.message.*;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Ture Bentzin
+ * 19.11.2022
+ */
+public class CoreMessagePostScript implements MessagePostScript {
+
+    private final Message message;
+    private final Date timeSent;
+    private final MessageRecipient messageRecipient;
+
+    public CoreMessagePostScript(Message message, MessageRecipient messageRecipient, Date timeSent) {
+        this.message = message;
+        this.timeSent = timeSent;
+        this.messageRecipient = messageRecipient;
+    }
+
+    public CoreMessagePostScript(Message message, MessageRecipient messageRecipient) {
+        this.message = message;
+        this.messageRecipient = messageRecipient;
+        this.timeSent = Date.from(Instant.now());
+    }
+
+
+    @Override
+    public Message message() {
+        return message;
+    }
+
+    @Override
+    public Date timeSent() {
+        return timeSent;
+    }
+
+    @Override
+    public MessageRecipient recipient() {
+        return messageRecipient;
+    }
+
+    @Override
+    public MultiMessagePostScript toMulti() {
+        return new CoreMultiMessagePostScript(List.of(message),List.of(recipient()), timeSent);
+    }
+}
