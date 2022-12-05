6114c7471e55df7ce6dd2ea6d83b83e2a198e8de
TDR <tdr.minecraft@gmail.com>
2022-12-05 11:09:28 +0100

    Update TODO.java

--- API/src/main/java/net/juligames/core/api/TODO.java
+++ API/src/main/java/net/juligames/core/api/TODO.java
@@ -4,6 +4,7 @@ package net.juligames.core.api;
  * @author Ture Bentzin
  * 20.11.2022
  * @apiNote Methods annotated with this might throw a {@link net.juligames.core.api.err.dev.TODOException}
+ * @suppression may suppress convert to record and unused
  */
 public @interface TODO {
     boolean doNotcall();
