cba87f3a87b512ee5f9b612b4e48e7cccc1ebe2d
TDR <tdr.minecraft@gmail.com>
2023-01-29 16:01:36 +0100

    add more security features to onEnable

--- PaperCore/src/main/java/net/juligames/core/paper/PaperCorePlugin.java
+++ PaperCore/src/main/java/net/juligames/core/paper/PaperCorePlugin.java
@@ -2,12 +2,15 @@ package net.juligames.core.paper;
 
 import net.juligames.core.Core;
 import net.juligames.core.adventure.AdventureCore;
+import net.juligames.core.api.API;
 import net.juligames.core.paper.events.ServerBootFinishedEvent;
 import net.juligames.core.paper.minigame.StartCommand;
 import net.juligames.core.paper.notification.EventNotificationListener;
+import net.juligames.core.paper.perms.PermissionCheckReturn;
 import net.juligames.core.paper.plugin.CorePluginLoadManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.List;
@@ -30,56 +33,61 @@ public class PaperCorePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        plugin = this;
-        Core core = new Core();
-        String serverName = Bukkit.getName() + "@" + ((Bukkit.getServer().getIp().isEmpty()) ? Bukkit.getServer().getIp() + ":" : Bukkit.getServer().getPort());
-        getLogger().info("starting core with the following identification: " + serverName);
-        core.start("paper-core|" + serverName);
-        //start adventureAPI
-        getLogger().info("starting adventureCore v." + AdventureCore.API_VERSION);
-        adventureCore = new AdventureCore();
-        adventureCore.start();
-        core.setOnlineRecipientProvider(() -> {
-            List<PaperMessageRecipient> paperMessageRecipients = new java.util.ArrayList<>(Bukkit.getOnlinePlayers().stream().map(PaperMessageRecipient::new).toList());
-            paperMessageRecipients.add(new PaperMessageRecipient(Bukkit.getConsoleSender()));
-            return paperMessageRecipients;
-        });
-
-        core.getCommandApi().setCommandHandler(s -> {
-            Bukkit.getScheduler().runTask(this, () -> {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), s);
+        try {
+            plugin = this;
+            Core core = new Core();
+            String serverName = Bukkit.getName() + "@" + ((Bukkit.getServer().getIp().isEmpty()) ? Bukkit.getServer().getIp() + ":" : Bukkit.getServer().getPort());
+            getLogger().info("starting core with the following identification: " + serverName);
+            core.start("paper-core|" + serverName);
+            //start adventureAPI
+            getLogger().info("starting adventureCore v." + AdventureCore.API_VERSION);
+            adventureCore = new AdventureCore();
+            adventureCore.start();
+            core.setOnlineRecipientProvider(() -> {
+                List<PaperMessageRecipient> paperMessageRecipients = new java.util.ArrayList<>(Bukkit.getOnlinePlayers().stream().map(PaperMessageRecipient::new).toList());
+                paperMessageRecipients.add(new PaperMessageRecipient(Bukkit.getConsoleSender()));
+                return paperMessageRecipients;
             });
 
-        });
+            core.getCommandApi().setCommandHandler(s -> {
+                Bukkit.getScheduler().runTask(this, () -> {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), s);
+                });
+            });
 
-        Objects.requireNonNull(getCommand("message")).setExecutor(new MessageCommand());
-        Objects.requireNonNull(getCommand("locale")).setExecutor(new LocaleCommand());
-        Objects.requireNonNull(getCommand("replacetest")).setExecutor(new ReplaceTestCommand());
-        Objects.requireNonNull(getCommand("bctest")).setExecutor(new BCTestCommand());
-        Objects.requireNonNull(getCommand("start")).setExecutor(new StartCommand());
-        Objects.requireNonNull(getCommand("printMessageCache")).setExecutor(new PrintMessageCacheCommand());
+            PermissionCheckReturn.registerMessages(API.get().getMessageApi());
 
-        Bukkit.getPluginManager().registerEvents(new PaperCoreEventListener(), this);
+            Objects.requireNonNull(getCommand("message")).setExecutor(new MessageCommand());
+            Objects.requireNonNull(getCommand("locale")).setExecutor(new LocaleCommand());
+            Objects.requireNonNull(getCommand("replacetest")).setExecutor(new ReplaceTestCommand());
+            Objects.requireNonNull(getCommand("bctest")).setExecutor(new BCTestCommand());
+            Objects.requireNonNull(getCommand("start")).setExecutor(new StartCommand());
+            Objects.requireNonNull(getCommand("printMessageCache")).setExecutor(new PrintMessageCacheCommand());
 
-        //Register NotificationEvent
-        core.getNotificationApi().registerListener(new EventNotificationListener());
+            Bukkit.getPluginManager().registerEvents(new PaperCoreEventListener(), this);
 
-        //Try to load miniGame (if present)
-        getServer().getScheduler().scheduleSyncDelayedTask(this, () -> Bukkit.getPluginManager().callEvent(new ServerBootFinishedEvent(core))); //experimental use of core
+            //Register NotificationEvent
+            core.getNotificationApi().registerListener(new EventNotificationListener());
 
-        //CorePlugin
-        {
-            final File file = Bukkit.getPluginsFolder();
-            corePluginLoadManager = new CorePluginLoadManager(file, Bukkit.getServer());
-            loadCorePlugins();
+            //Try to load miniGame (if present)
+            getServer().getScheduler().scheduleSyncDelayedTask(this, () -> Bukkit.getPluginManager().callEvent(new ServerBootFinishedEvent(core))); //experimental use of core
+
+            //CorePlugin
+            {
+                final File file = Bukkit.getPluginsFolder();
+                corePluginLoadManager = new CorePluginLoadManager(file, Bukkit.getServer());
+                loadCorePlugins();
+            }
+        } catch (Exception e) {
+            Core core = Core.getInstance();
+            core.getCoreLogger().info(e + " -> Client shutdown!");
+            killClient(core);
+            throw new RuntimeException(e);
         }
 
     }
 
-    @Override
-    public void onDisable() {
-        Core core = Core.getInstance();
-        core.getCoreLogger().info("onDisable() -> Client shutdown!");
+    protected void killClient(@NotNull Core core) {
         adventureCore.dropApiService();
         //Try to abort miniGame (if present)
         core.getLocalMiniGame().ifPresent(basicMiniGame -> {
@@ -87,6 +95,13 @@ public class PaperCorePlugin extends JavaPlugin {
             basicMiniGame.abort();
         });
         core.stop();
+    }
+
+    @Override
+    public void onDisable() {
+        Core core = Core.getInstance();
+        core.getCoreLogger().info("onDisable() -> Client shutdown!");
+        killClient(core);
     }
 
     private void loadCorePlugins() {
