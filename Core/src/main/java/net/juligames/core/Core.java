287a409adcb98e1191d20d5a96dae9b12706823c
TDR <tdr.minecraft@gmail.com>
2023-02-10 17:27:18 +0100

    bump 1.3-SNAPSHOT

--- Core/src/main/java/net/juligames/core/Core.java
+++ Core/src/main/java/net/juligames/core/Core.java
@@ -48,9 +48,9 @@ public final class Core implements API {
      * This can be set depending on the build of the Core
      */
     public static final String CORE_BRAND = "Core";
-    public static final String CORE_VERSION_NUMBER = "1.2";
+    public static final String CORE_VERSION_NUMBER = "1.3-SNAPSHOT";
     public static final String CORE_SPECIFICATION = "Gustav";
-    private static final String BUILD_VERSION = "1.2"; //POM VERSION
+    private static final String BUILD_VERSION = "1.3-SNAPSHOT"; //POM VERSION
     
     private static Core core;
     private final Registerator<Consumer<HazelcastInstance>> hazelcastPostPreparationWorkers = new Registerator<>("hazelcastPostPreparationWorkers");
