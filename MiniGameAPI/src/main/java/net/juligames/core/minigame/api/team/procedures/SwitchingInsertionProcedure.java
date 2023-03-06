5c80025dd2ef30f3ed6f361e7dd28a3fa7b809fc
Ture Bentzin <Tur.Ben@mgm-monschau.eu>
2023-03-06 13:54:08 +0100

    start fixing the SwitchingInsertionProcedure

--- MiniGameAPI/src/main/java/net/juligames/core/minigame/api/team/procedures/SwitchingInsertionProcedure.java
+++ MiniGameAPI/src/main/java/net/juligames/core/minigame/api/team/procedures/SwitchingInsertionProcedure.java
@@ -24,10 +24,16 @@ public final class SwitchingInsertionProcedure implements InsertionProcedure {
     @Contract(pure = true)
     @Override
     public @NotNull Boolean apply(@NotNull Set<Team> teams, UUID uuid) {
+        if (lastTeam == null) {
+            teams.stream().findAny().ifPresentOrElse(team -> lastTeam = team,
+                    () -> {
+                        throw new IllegalArgumentException("lastTeam is null and there is no team available to be selected!");
+                    });
+        }
         if (!teams.contains(lastTeam)) {
             throw new IllegalArgumentException("lastTeam of this SwitchingInsertionProcedure is not part of teams!");
         }
-        return new ComparatorInsertionProcedure((o1, o2) -> { //needs testing - maybe reversed order
+        return new ComparatorInsertionProcedure((o1, o2) -> { //needs testing - maybe reversed order - not running - no feedback on what team was selected...
             if (o2 == lastTeam) return 1;
             if (o2 == o1 || o2.equals(o1)) return 0;
             return -1;
