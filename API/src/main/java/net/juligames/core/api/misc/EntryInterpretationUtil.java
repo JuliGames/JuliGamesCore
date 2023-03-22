85474c94b9ad8853b3bcc277d0cb43fa13e6bdd2
Ture Bentzin <Tur.Ben@mgm-monschau.eu>
2023-03-22 12:56:28 +0100

    fix exception handeling with entry interpretation

--- API/src/main/java/net/juligames/core/api/misc/EntryInterpretationUtil.java
+++ API/src/main/java/net/juligames/core/api/misc/EntryInterpretationUtil.java
@@ -21,18 +21,12 @@ public class EntryInterpretationUtil {
     }
 
     @Contract("_, _, _ -> new")
-    public static <K, V> Map.@NotNull @Unmodifiable Entry<String, String> reverseEntry(Map.@NotNull Entry<K, V> stringStringEntry, @NotNull Interpreter<K> kInterpreter, @NotNull Interpreter<V> vInterpreter) throws Exception {
+    public static <K, V> Map.@NotNull @Unmodifiable Entry<String, String> reverseEntry(Map.@NotNull Entry<K, V> stringStringEntry, @NotNull Interpreter<K> kInterpreter, @NotNull Interpreter<V> vInterpreter) {
         return Map.entry(kInterpreter.reverse(stringStringEntry.getKey()), vInterpreter.reverse(stringStringEntry.getValue()));
     }
 
     public static <K, V> @Unmodifiable Collection<Map.Entry<String, String>> reverseEntries(@NotNull Collection<Map.Entry<K,V>> collection, Interpreter<K> kInterpreter, Interpreter<V> vInterpreter) {
-        return collection.stream().map(kvEntry -> {
-            try {
-                return reverseEntry(kvEntry,kInterpreter,vInterpreter);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }).collect(Collectors.toUnmodifiableSet());
+        return collection.stream().map(kvEntry -> reverseEntry(kvEntry,kInterpreter,vInterpreter)).collect(Collectors.toUnmodifiableSet());
     }
 
 
