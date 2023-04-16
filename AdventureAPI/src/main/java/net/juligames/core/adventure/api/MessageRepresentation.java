091cdc5baab11a2acf7694499e63dee78b6a6daf
TDR <tdr.minecraft@gmail.com>
2023-04-16 21:39:12 +0200

    added option to interpret maps

--- AdventureAPI/src/main/java/net/juligames/core/adventure/api/MessageRepresentation.java
+++ AdventureAPI/src/main/java/net/juligames/core/adventure/api/MessageRepresentation.java
@@ -2,12 +2,11 @@ package net.juligames.core.adventure.api;
 
 import net.juligames.core.adventure.AdventureTagManager;
 import net.juligames.core.api.API;
+import net.juligames.core.api.config.BuildInInterpreters;
 import net.juligames.core.api.jdbi.DBLocale;
-import net.juligames.core.api.jdbi.DBMessage;
 import net.juligames.core.api.message.Message;
-import net.kyori.adventure.audience.Audience;
+import net.juligames.core.api.misc.EntryInterpretationUtil;
 import net.kyori.adventure.identity.Identity;
-import net.kyori.adventure.pointer.Pointer;
 import net.kyori.adventure.pointer.Pointered;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.ComponentLike;
@@ -15,7 +14,12 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * @author Ture Bentzin
@@ -26,6 +30,7 @@ public class MessageRepresentation implements ComponentLike {
 
     private final @NotNull AdventureTagManager adventureTagManager;
     private final @NotNull Message message;
+
     public MessageRepresentation(@NotNull AdventureTagManager adventureTagManager, @NotNull Message message) {
         this.adventureTagManager = adventureTagManager;
         this.message = message;
@@ -79,7 +84,9 @@ public class MessageRepresentation implements ComponentLike {
         return new MessageRepresentation(API.get().getMessageApi().getMessageSmart(messageKey, pointered.get(Identity.LOCALE).orElse(defaultLocale()), replacements));
     }
 
-
+    private static @NotNull Locale defaultLocale() {
+        return API.get().getMessageApi().defaultUtilLocale();
+    }
 
     @Override
     public @NotNull Component asComponent() {
@@ -90,7 +97,14 @@ public class MessageRepresentation implements ComponentLike {
         return message;
     }
 
-    private static @NotNull Locale defaultLocale() {
-        return API.get().getMessageApi().defaultUtilLocale();
+    private void demo() {
+
+        Map<File, URL> fileURLMap = new HashMap<>();
+        Map<String, String> stringStringMap = EntryInterpretationUtil.reverseMap(fileURLMap, BuildInInterpreters.fileInterpreter(), BuildInInterpreters.urlInterpreter());
+        Map<File, URL> fileURLMap1 = EntryInterpretationUtil.interpretMap(stringStringMap, BuildInInterpreters.fileInterpreter(), BuildInInterpreters.urlInterpreter());
+        assert fileURLMap1.equals(fileURLMap);
+
+        Stream<Map.Entry<String, String>> entryStream = fileURLMap.entrySet().stream().map(fileURLEntry -> EntryInterpretationUtil.reverseEntry(fileURLEntry, BuildInInterpreters.fileInterpreter(), BuildInInterpreters.urlInterpreter()));
+
     }
 }
